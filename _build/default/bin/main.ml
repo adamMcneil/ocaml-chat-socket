cd76c5eb75bc901eaf9920e0393428db0938fbc5
adamwm4 <adamwm4@illinois.edu>
2025-01-22 10:31:56 -0600

    need to add peek

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -5,6 +5,8 @@ let host = "127.0.0.1"
 let port = 8080
 let ack_message = "###ack###"
 let max_message_size = 1024
+(* let messages = [] *)
+(* let messages_mutex = Mutex.create () *)
 
 let send_messages socket =
   let rec loop message =
@@ -17,28 +19,39 @@ let send_messages socket =
       let end_time = Unix.gettimeofday () in
       let elapsed_time = end_time -. start_time in
       Printf.printf "Server response: %.6f seconds\n%!" elapsed_time;
+      let buffer_string = Bytes.to_string buffer in
+      print_endline buffer_string;
       let next_message = read_line () in
       loop next_message)
   in
   let message = read_line () in
   loop message;
+  Printf.printf "closing socket%!";
   close socket
 
 let recv_messages socket =
   let buffer = Bytes.create max_message_size in
-  while true do
+  let rec loop () =
     let bytes_read = read socket buffer 0 max_message_size in
     let message = Bytes.sub_string buffer 0 bytes_read in
     match message with
-    | "###ack###" -> Printf.printf "recieved ack at the wrong time\n"
+    (* | "###ack###" -> *)
+    (*     Printf.printf "recieved ack at the wrong time\n%!"; *)
+    (*     loop () *)
+    | "" ->
+        Printf.printf "closing recv ending%!";
+        close socket
     | _ ->
-        Printf.printf "Received: %s\n%!" message;
-        ignore
-          (write socket
-             (Bytes.of_string ack_message)
-             0
-             (String.length ack_message))
-  done
+        Printf.printf "Received: \"%s\"\n%!" message;
+        let _ =
+          write socket
+            (Bytes.of_string ack_message)
+            0
+            (String.length ack_message)
+        in
+        loop ()
+  in
+  loop ()
 
 let start_server port =
   let socket_address = ADDR_INET (inet_addr_any, port) in
@@ -46,15 +59,18 @@ let start_server port =
   bind socket socket_address;
   listen socket 1;
   Printf.printf "Server listening on port %d\n%!" port;
-  while true do
+  let rec loop () =
     let connected_socket, client_addr = accept socket in
-    ignore (create send_messages connected_socket);
+    let _ = create send_messages connected_socket in
     Printf.printf "Client connected: %s\n%!"
       (match client_addr with
       | ADDR_INET (addr, _) -> string_of_inet_addr addr
       | _ -> "Unknown");
-    ignore (recv_messages connected_socket)
-  done
+    let _ = recv_messages connected_socket in
+    Printf.printf "Disconnected\n%!";
+    loop ()
+  in
+  loop ()
 
 let connect_to_server host port =
   let socket_address = ADDR_INET (inet_addr_of_string host, port) in
