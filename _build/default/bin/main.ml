3b9d3b48cf1e418e5746d0c3c8cda3e0599ac4fc
adamwm4 <adamwm4@illinois.edu>
2025-01-23 09:12:36 -0600

    reformated

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -4,77 +4,69 @@ open Thread
 let host = "127.0.0.1"
 let port = 8080
 let ack_message = "###ack###"
-let max_message_size = 1024
+let max_message_size = 102
 let messages_mutex = Mutex.create ()
 
 let send_messages socket =
-  let rec loop message =
+  let rec send_message_loop message =
     if message <> "quit" then (
       Mutex.lock messages_mutex;
       let start_time = Unix.gettimeofday () in
-      ignore
-        (Unix.write socket (Bytes.of_string message) 0 (String.length message));
+      ignore (write socket (Bytes.of_string message) 0 (String.length message));
       let buffer = Bytes.create max_message_size in
-      let _ = Unix.read socket buffer 0 max_message_size in
+      ignore (read socket buffer 0 max_message_size);
       let end_time = Unix.gettimeofday () in
       let elapsed_time = end_time -. start_time in
-      Printf.printf "Server response: %.6f seconds\n%!" elapsed_time;
-      (* let buffer_string = Bytes.to_string buffer in *)
-      (* print_endline buffer_string; *)
+      Printf.printf "%.6f seconds roundtrip latency\n%!" elapsed_time;
       Mutex.unlock messages_mutex;
+
       let next_message = read_line () in
-      loop next_message)
+      send_message_loop next_message)
   in
   let message = read_line () in
-  loop message;
-  Printf.printf "closing socket\n%!";
-  close socket
+  send_message_loop message
 
-let is_ready_to_read sock =
-  let read_fds, _, _ = Unix.select [ sock ] [] [] 0.0 in
+let ready_to_read socket =
+  let read_fds, _, _ = Unix.select [ socket ] [] [] 0.0 in
   read_fds <> []
 
 let recv_messages socket =
   let buffer = Bytes.create max_message_size in
-  let rec loop () =
+  let rec recv_message_loop () =
     Mutex.lock messages_mutex;
-    if is_ready_to_read socket then (
+    if ready_to_read socket then (
       let bytes_read = read socket buffer 0 max_message_size in
-      Mutex.unlock messages_mutex;
       let message = Bytes.sub_string buffer 0 bytes_read in
-      match message with
-      | "" ->
-          Printf.printf "closing recv ending\n%!";
-          close socket
-      | _ ->
-          Printf.printf "Received: \"%s\"\n%!" message;
-          let _ =
-            write socket
-              (Bytes.of_string ack_message)
-              0
-              (String.length ack_message)
-          in
-          loop ())
+      Printf.printf "Received: \"%s\"\n%!" message;
+      ignore
+        (write socket
+           (Bytes.of_string ack_message)
+           0
+           (String.length ack_message));
+      Mutex.unlock messages_mutex;
+      recv_message_loop ())
     else Mutex.unlock messages_mutex;
-    loop ()
+    recv_message_loop ()
   in
-  loop ()
+  recv_message_loop ()
 
 let start_server port =
   let socket_address = ADDR_INET (inet_addr_any, port) in
   let socket = socket PF_INET SOCK_STREAM 0 in
   bind socket socket_address;
   listen socket 1;
-  Printf.printf "Server listening on port %d\n%!" port;
   let rec loop () =
+    Printf.printf "Server listening on port %d\n%!" port;
     let connected_socket, client_addr = accept socket in
     let _ = create send_messages connected_socket in
-    Printf.printf "Client connected: %s\n%!"
-      (match client_addr with
+    let client =
+      match client_addr with
       | ADDR_INET (addr, _) -> string_of_inet_addr addr
-      | _ -> "Unknown");
+      | _ -> "Unknown"
+    in
+    Printf.printf "Client connected: %s\n%!" client;
     let _ = recv_messages connected_socket in
-    Printf.printf "Disconnected\n%!";
+    Printf.printf "Client disconnected: %s\n%!" client;
     loop ()
   in
   loop ()
