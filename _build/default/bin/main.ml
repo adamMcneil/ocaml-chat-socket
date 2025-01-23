092b97d5a1d5f6651f61cf3fe8ac886ee89fbe01
adamwm4 <adamwm4@illinois.edu>
2025-01-23 13:20:24 -0600

    changed names

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -2,15 +2,15 @@ open Unix
 open Thread
 
 let default_host = "127.0.0.1"
-let default_port = 8080
+let port = 8080
 let ack_message = "###ack###"
 let max_message_size = 1024
-let messages_mutex = Mutex.create ()
+let mutex = Mutex.create ()
 
 let send_messages socket =
   let rec send_message_loop message =
     if message <> "quit" then (
-      Mutex.lock messages_mutex;
+      Mutex.lock mutex;
       let start_time = Unix.gettimeofday () in
       ignore (write socket (Bytes.of_string message) 0 (String.length message));
       let buffer = Bytes.create max_message_size in
@@ -18,7 +18,7 @@ let send_messages socket =
       let end_time = Unix.gettimeofday () in
       let elapsed_time = end_time -. start_time in
       Printf.printf "%.6f seconds roundtrip latency\n%!" elapsed_time;
-      Mutex.unlock messages_mutex;
+      Mutex.unlock mutex;
 
       let next_message = read_line () in
       send_message_loop next_message)
@@ -34,23 +34,24 @@ let ready_to_read socket =
 let recv_messages socket =
   let buffer = Bytes.create max_message_size in
   let rec recv_message_loop () =
-    Mutex.lock messages_mutex;
+    Mutex.lock mutex;
     if ready_to_read socket then (
       let bytes_read = read socket buffer 0 max_message_size in
-      let message = Bytes.sub_string buffer 0 bytes_read in
+      Printf.printf "Length of message: %d\n%!" bytes_read;
+      let message = Bytes.to_string buffer in
       Printf.printf "Received: \"%s\"\n%!" message;
       ignore
         (write socket
            (Bytes.of_string ack_message)
            0
            (String.length ack_message));
-      Mutex.unlock messages_mutex;
+      Mutex.unlock mutex;
       recv_message_loop ())
-    else Mutex.unlock messages_mutex;
+    else Mutex.unlock mutex;
     recv_message_loop ()
   in
   try recv_message_loop () with
-  | Unix.Unix_error (Unix.EBADF, "select", _) -> Mutex.unlock messages_mutex
+  | Unix.Unix_error (Unix.EBADF, "select", _) -> Mutex.unlock mutex
   | e -> raise e
 
 let start_server port =
@@ -83,23 +84,14 @@ let connect_to_server host port =
   send_messages socket
 
 let () =
-  print_string "Do you want to be a server or client? (s/c)";
+  print_string "Do you want to be a server or client? (s/c) ";
   let server_option = read_line () in
   match server_option with
-  | "s" -> (
-      Printf.printf "Enter port to start server on: ";
-      let port = read_line () in
-      match port with
-      | "" -> start_server default_port
-      | _ -> start_server (int_of_string port))
+  | "s" -> start_server port
   | "c" -> (
       Printf.printf "Enter host to connect to: ";
       let host = read_line () in
-      Printf.printf "Enter port to connect to: ";
-      let port = read_line () in
-      match (port, host) with
-      | "", "" -> connect_to_server default_host default_port
-      | _, "" -> connect_to_server host default_port
-      | "", _ -> connect_to_server default_host (int_of_string port)
-      | _, _ -> connect_to_server host (int_of_string port))
+      match host with
+      | "" -> connect_to_server default_host port
+      | _ -> connect_to_server host port)
   | _ -> Printf.printf "not a valid option"
