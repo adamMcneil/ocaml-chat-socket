c1864ff4ed53595bb86ba875a249321c551cea0f
adamwm4 <adamwm4@illinois.edu>
2025-01-24 10:11:47 -0600

    added socket status

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -6,53 +6,76 @@ let port = 8080
 let ack_message = "###ack###"
 let max_message_size = 1024
 let mutex = Mutex.create ()
+let quit_flag = ref false
+
+let rec enter_message () =
+  let message = read_line () in
+  match message with "" -> enter_message () | _ -> message
 
 let send_messages socket =
   let rec send_message_loop message =
-    if message <> "quit" then (
+    if message <> "quit" || !quit_flag then (
       Mutex.lock mutex;
       let start_time = Unix.gettimeofday () in
       ignore (write socket (Bytes.of_string message) 0 (String.length message));
       let buffer = Bytes.create max_message_size in
-      ignore (read socket buffer 0 max_message_size);
+      let x = read socket buffer 0 max_message_size in
       let end_time = Unix.gettimeofday () in
       let elapsed_time = end_time -. start_time in
       Printf.printf "%.6f seconds roundtrip latency\n%!" elapsed_time;
+      Printf.printf "%d\n%!" x;
       Mutex.unlock mutex;
 
-      let next_message = read_line () in
+      let next_message = enter_message () in
       send_message_loop next_message)
   in
   let message = read_line () in
-  send_message_loop message;
-  close socket
+  (try send_message_loop message
+   with _ ->
+     Printf.printf "error in sending messages";
+     Mutex.unlock mutex);
+  let _ = quit_flag = ref true in
+  close socket;
+  Printf.printf "closed"
+(* shutdown socket SHUTDOWN_SEND *)
 
-let ready_to_read socket =
-  let read_fds, _, _ = Unix.select [ socket ] [] [] 0.0 in
-  read_fds <> []
+type socket_status = Closed | Empty | Full
+
+let is_socket_closed socket_fd =
+  let read_ready, _, _ = Unix.select [ socket_fd ] [] [] 0.0 in
+  if read_ready = [] then Empty
+  else
+    try
+      let buffer = Bytes.create 1 in
+      let bytes_peeked = Unix.recv socket_fd buffer 0 1 [ Unix.MSG_PEEK ] in
+      if bytes_peeked = 0 (* If 0 bytes, the connection is closed *) then Closed
+      else Full
+    with
+    | Unix.Unix_error (Unix.EAGAIN, _, _) ->
+        Empty (* No data yet, but connection open *)
+    | _ -> Closed (* Treat unexpected errors as connection closed *)
 
 let recv_messages socket =
   let buffer = Bytes.create max_message_size in
   let rec recv_message_loop () =
-    Mutex.lock mutex;
-    if ready_to_read socket then (
-      let bytes_read = read socket buffer 0 max_message_size in
-      Printf.printf "Length of message: %d\n%!" bytes_read;
-      let message = Bytes.to_string buffer in
-      Printf.printf "Received: \"%s\"\n%!" message;
-      ignore
-        (write socket
-           (Bytes.of_string ack_message)
-           0
-           (String.length ack_message));
-      Mutex.unlock mutex;
-      recv_message_loop ())
-    else Mutex.unlock mutex;
-    recv_message_loop ()
+    match is_socket_closed socket with
+    | Closed -> quit_flag = ref true
+    | Empty -> recv_message_loop ()
+    | Full ->
+        Mutex.lock mutex;
+        let bytes_read = read socket buffer 0 max_message_size in
+        Printf.printf "Length of message: %d\n%!" bytes_read;
+        let message = Bytes.to_string buffer in
+        Printf.printf "Received: \"%s\"\n%!" message;
+        ignore
+          (write socket
+             (Bytes.of_string ack_message)
+             0
+             (String.length ack_message));
+        Mutex.unlock mutex;
+        recv_message_loop ()
   in
-  try recv_message_loop () with
-  | Unix.Unix_error (Unix.EBADF, "select", _) -> Mutex.unlock mutex
-  | e -> raise e
+  recv_message_loop ()
 
 let start_server port =
   let socket_address = ADDR_INET (inet_addr_any, port) in
@@ -70,6 +93,7 @@ let start_server port =
     in
     Printf.printf "Client connected: %s\n%!" client;
     let _ = recv_messages connected_socket in
+
     Printf.printf "Client disconnected: %s\n%!" client;
     accept_clients_loop ()
   in
